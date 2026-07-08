675c05ee073cb42edbd1c274064cd96b513b6b9b
Fermin99-ctrl <millanaof38@gmail.com>
2026-07-08 03:27:51 -0400

    cambios en ruter para asistencia

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -9,7 +9,6 @@ import inscripcionRoutes from "./Inscripcion.routes.js";
 import ventaRoutes from "./venta.routes.js";
 import reservaRoutes from "./reserva.routes.js";
 import vehiculoRoutes from "./vehiculo.routes.js";
-import asistenciaRoutes from "./asistencia.routes.js";
 
 import registroEsperaRoutes from "./registroEspera.routes.js";
 import dashboardRoutes from "./dashboard.routes.js";
@@ -27,7 +26,6 @@ router.use("/users", userRoutes)
 router.use("/ventas", ventaRoutes);
 router.use("/reservas", reservaRoutes);
 router.use("/vehiculos", vehiculoRoutes);
-router.use("/asistencia", asistenciaRoutes);
 
 router.use("/registro-espera", registroEsperaRoutes);
 router.use("/dashboard", dashboardRoutes);
